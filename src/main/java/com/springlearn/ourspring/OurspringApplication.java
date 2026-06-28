79110dd80bc2318259d1acc10dc733ca18db0fbe
Deepak <deepak.siva1981@gmail.com>
2026-06-28 16:23:31 +0530

    Cool new

--- src/main/java/com/springlearn/ourspring/OurspringApplication.java
+++ src/main/java/com/springlearn/ourspring/OurspringApplication.java
@@ -1,13 +1,22 @@
 package com.springlearn.ourspring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = {"com.springlearn.ourspring", "com.springlearn.utils"})
 public class OurspringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OurspringApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> {
+			System.out.println("Hello World!!");
+		};
 	}
 
 }
---
+++ src/main/java/com/springlearn/ourspring/entity/Student.java
@@ -0,0 +1,5 @@
+package com.springlearn.ourspring.entity;
+
+public class Student {
+
+}
